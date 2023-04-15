3c4dd885a26683b9edc186abd263fbd04b43fd4b
Vivek Shinde <vivshind@publicisgroupe.net>
2023-04-15 12:35:54 +0530

    Added basic authentication

--- src/main/java/com/blogapi/bloggingapi/config/SecurityConfig.java
+++ src/main/java/com/blogapi/bloggingapi/config/SecurityConfig.java
@@ -1,14 +1,39 @@
 package com.blogapi.bloggingapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.blogapi.bloggingapi.security.CustomUserDetailService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    // custom user detail service => to get access for role authentication
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
+    // password encoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // dao authentication provider
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(this.customUserDetailService);
+        provider.setPasswordEncoder(this.passwordEncoder());
+        return provider;
+    }
+
     // http configuration
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -20,6 +45,8 @@ public class SecurityConfig {
                 .authenticated()
                 .and()
                 .httpBasic();
+        http.authenticationProvider(this.daoAuthenticationProvider());
         return http.build();
     }
+
 }
--- src/main/java/com/blogapi/bloggingapi/security/CustomUserDetailService.java
+++ src/main/java/com/blogapi/bloggingapi/security/CustomUserDetailService.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.blogapi.bloggingapi.entities.User;
 import com.blogapi.bloggingapi.exceptions.ResourceNotFoundException;
 import com.blogapi.bloggingapi.repositories.UserRepository;
 
+@Service
 public class CustomUserDetailService implements UserDetailsService {
     @Autowired
     private UserRepository _userRepository;
