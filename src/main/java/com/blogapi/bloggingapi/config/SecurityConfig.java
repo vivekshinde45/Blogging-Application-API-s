168db96e08ce558b7b6c13c8381dc1eb7feb82da
Vivek Shinde <vivshind@publicisgroupe.net>
2023-04-15 10:13:12 +0530

    Added security dependency and basic authentication

---
+++ src/main/java/com/blogapi/bloggingapi/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.blogapi.bloggingapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    // http configuration
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+}
